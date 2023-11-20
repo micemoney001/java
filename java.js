7c080287618223313ddd7aa5fe9ce5d384e0c17d
Temidayo0 <148203679+Temidayo0@users.noreply.github.com>
2023-11-20 11:59:00 -0800

    commoit

---
+++ java.js
@@ -0,0 +1,14 @@
+function wel() {
+    document.querySelector('p').innerHTML = 'WHAT CAN WE DO FOR YOU'
+    let name = 65;
+    let Name = 60;
+    alert(name + Name);
+
+
+}
+function nh() {
+    document.write('MY NAME IS MAYOWA');
+}
+function lo() {
+    confirm('SAVE ACCOUNT');
+}
